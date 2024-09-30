aab4777cbe3f7103735cffdecc46c57443704065
Simone Riggi <simone.riggi@gmail.com>
2024-09-30 19:23:07 +0200

    Refactor llava ov inference, add smorph task

--- radio_llava/inference_utils.py
+++ radio_llava/inference_utils.py
@@ -71,11 +71,23 @@ def process_model_output_multiclass_singlelabel(model_output, label, label2id, l
 def filter_smorph_label(labels):
 	""" Modifies the input labels for smorph dataset """
 
-	nlabels= len(labels)
-	if nlabels==1 and (labels[0]=="COMPACT" or labels[0]=="BACKGROUND"):
-		labels[0]= "NONE"
-		
-	return labels
+	# - First add EXTENDED label if RADIO-GALAXY is present
+	labels_sel= labels.copy()
+	if "RADIO-GALAXY" in labels_sel and "EXTENDED" not in labels_sel:
+		labels_sel.append("EXTENDED")
+	
+	# - Set label to NONE if only COMPACT/BACKGROUND labels are given
+	nlabels= len(labels_sel)
+	if nlabels==1 and (labels_sel[0]=="COMPACT" or labels_sel[0]=="BACKGROUND"):
+		labels_sel[0]= "NONE"
+	
+	# - Remove undesired labels
+	labels_to_be_removed= ["COMPACT","BACKGROUND","RADIO-GALAXY","DUBIOUS","WTF","FILAMENT","RING","ARC","ARTEFACT","BORDER","MOSAICING"]
+	for item in labels_sel:
+		if item in labels_to_be_removed:
+			labels_sel.remove(item)
+	
+	return labels_sel
 	
 
 def process_model_output_multiclass_multilabel(model_output, model_output, label, label2id, label_modifier_fcn=None):
@@ -87,7 +99,11 @@ def process_model_output_multiclass_multilabel(model_output, model_output, label
 	mlb = MultiLabelBinarizer(classes=np.arange(0, nclasses))
 
 	# - Process ground truth label (label is in this case a string container labels separated by commas)
-	labels= [str(x.strip()) for x in label.split(',')]
+	#labels= [str(x.strip()) for x in label.split(',')]
+	#if label_modifier_fcn is not None:
+	#	labels= label_modifier_fcn(labels)
+	
+	# - Process ground truth label (label is in this case a list with labels)
 	if label_modifier_fcn is not None:
 		labels= label_modifier_fcn(labels)
 		
