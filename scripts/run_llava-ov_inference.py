e2a962238560411c1b4c3079b2c8a1f178ca5128
Simone Riggi <simone.riggi@gmail.com>
2025-01-23 09:40:33 +0100

    Add galaxy MNIST benchmark

--- scripts/run_llava-ov_inference.py
+++ scripts/run_llava-ov_inference.py
@@ -311,6 +311,25 @@ def main():
 			conv_template=args.conv_template,
 			verbose=args.verbose
 		)
+	
+	elif args.benchmark=="galaxymorphclass-gmnist":
+		logger.info("Running galaxymorphclass-gmnist benchmark inference ...")
+		run_llavaov_model_gmnist_inference(
+			datalist=datalist,
+			model=model,
+			tokenizer=tokenizer,
+			image_processor=image_processor,
+			datalist_context=datalist_context,
+			device=device,
+			resize=args.resize, resize_size=args.imgsize, 
+			zscale=args.zscale, contrast=args.contrast,
+			shuffle_options=args.shuffle_options,
+			nmax=args.nmax,
+			nmax_context=args.nmax_context,
+			add_task_description=args.add_task_description,
+			conv_template=args.conv_template,
+			verbose=args.verbose
+		)
 		
 	else:
 		logger.error("Unknown/invalid benchmark (%s) given!" % (args.benchmark))
